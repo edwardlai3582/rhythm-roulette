1db5b4bbcb7d928c39930d2a84fd860936fa7289
illl48 <mruncanny@gmail.com>
2016-08-06 04:37:55 -0700

    iphone and css

--- app/components/fullSizeImgWithText/index.js
+++ app/components/fullSizeImgWithText/index.js
@@ -30,14 +30,25 @@ export class FullSizeImgWithText extends React.Component { // eslint-disable-lin
     };
 
     render() {
-        return (
-            <section className={styles.imgWrapper} ref="myText">  
-                <video loop autoPlay="autoplay" poster={BG}  className={styles.bg}>
-                  <source src="https://firebasestorage.googleapis.com/v0/b/rhythmroulette-78b71.appspot.com/o/rropening3.mp4?alt=media&token=a2e6140a-242f-4cdb-b616-a658b8b52dd5" type="video/mp4" />
-                </video>
-                <img src={ARROW} alt="scroll down" className={styles.arrow} onClick={this.goDown} />
-            </section>
-        );
+        if(navigator.userAgent.match(/iPhone|iPod|Android/i)){
+            console.log("APPLE!");
+            return (
+                <section className={styles.imgWrapper} ref="myText">  
+                    <img src={BG} alt="scroll down" className={styles.appleBg} />
+                    <img src={ARROW} alt="scroll down" className={styles.arrow} onClick={this.goDown} />
+                </section>
+            );            
+        }
+        else {
+            return (
+                <section className={styles.imgWrapper} ref="myText">  
+                    <video loop autoPlay="autoplay" poster={BG}  className={styles.bg}>
+                      <source src="https://firebasestorage.googleapis.com/v0/b/rhythmroulette-78b71.appspot.com/o/rropening3.mp4?alt=media&token=a2e6140a-242f-4cdb-b616-a658b8b52dd5" type="video/mp4" />
+                    </video>
+                    <img src={ARROW} alt="scroll down" className={styles.arrow} onClick={this.goDown} />
+                </section>
+            );
+        }    
     }
 }
 
