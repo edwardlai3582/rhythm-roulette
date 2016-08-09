d3db4831251bba2947f4673772c5356d3b92e772
illl48 <mruncanny@gmail.com>
2016-08-09 03:17:01 -0700

    what

--- app/firebase/config.js
+++
@@ -1,7 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyABOvyyjtmu4ioGemSRy4SJcjtBU5DUsqA",
-    authDomain: "rhythmroulette-78b71.firebaseapp.com",
-    databaseURL: "https://rhythmroulette-78b71.firebaseio.com",
-    storageBucket: "rhythmroulette-78b71.appspot.com"
-};
-   
