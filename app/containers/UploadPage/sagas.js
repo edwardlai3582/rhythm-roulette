5b0469807aa532a973bf877ad8cd8ccd659eba17
illl48 <mruncanny@gmail.com>
2016-08-09 00:51:21 -0700

    add firebase folder

--- app/containers/UploadPage/sagas.js
+++ app/containers/UploadPage/sagas.js
@@ -1,62 +1,88 @@
-/**
- * Gets the repositories of the user from Github
- */
-
 import { take, call, put, select, fork, cancel } from 'redux-saga/effects';
 import { LOCATION_CHANGE } from 'react-router-redux';
 import { LOAD_RRSFORSORT } from 'containers/App/constants';
-import { rrsForSortLoaded, rrsForSortLoadingError } from 'containers/App/actions';
+//import { rrsForSortLoaded, rrsForSortLoadingError } from 'containers/App/actions';
+import 'firebase';
 
 import request from 'utils/request';
 
-import { getAll, get } from 'firebase-saga';
-
+/*
 export function* getEps() {
     const rhythmroulettes = yield call(getAll, 'rhythmroulettes');
 
     const youtubeUrl = "https://edwardlai3582.com/youtube?id="
-    /*
-    for(let i=0; i<rhythmroulettes.length; i++){
-        let youtubeData = yield call(request, youtubeUrl+rhythmroulettes[i].youtubeId);
-        console.log(youtubeData);
-        rhythmroulettes[i].youtubeData = youtubeData;
-    }  
-    */
     let callArray=[];
     for(let i=0; i<rhythmroulettes.length; i++){
         callArray.push(call(request, youtubeUrl+rhythmroulettes[i].youtubeId));
     }      
-    
     let callArrayResult = yield callArray;
     for(var i=0; i<callArrayResult.length; i++){
         rhythmroulettes[i].youtubeData = callArrayResult[i];    
     }
-    
     yield put(rrsForSortLoaded({rhythmroulettes:rhythmroulettes}));
 }
-
-/**
- * Watches for LOAD_REPOS action and calls handler
- */
 export function* getEpsWatcher() {
   while (yield take(LOAD_RRSFORSORT)) {
     yield call(getEps);
   }
 }
-
-/**
- * Root saga manages watcher lifecycle
- */
 export function* epsData() {
-  // Fork watcher so we can continue execution
   const watcher = yield fork(getEpsWatcher);
-
-  // Suspend execution until location changes
   yield take(LOCATION_CHANGE);
   yield cancel(watcher);
 }
 
-// Bootstrap sagas
 export default [
   epsData    
+];
+*/
+function* signIn(authProvider) {
+  try {
+    const authData = yield call([firebaseAuth, firebaseAuth.signInWithPopup], authProvider);
+    yield put(authActions.signInFulfilled(authData.user));
+    yield history.push('/');
+  }
+  catch (error) {
+    yield put(authActions.signInFailed(error));
+  }
+}
+
+function* signOut() {
+  try {
+    yield call([firebaseAuth, firebaseAuth.signOut]);
+    yield put(authActions.signOutFulfilled());
+    yield history.replace('/sign-in');
+  }
+  catch (error) {
+    yield put(authActions.signOutFailed(error));
+  }
+}
+
+
+//=====================================
+//  WATCHERS
+//-------------------------------------
+
+function* watchSignIn() {
+  while (true) {
+    let { payload } = yield take(authActions.SIGN_IN);
+    yield fork(signIn, payload.authProvider);
+  }
+}
+
+function* watchSignOut() {
+  while (true) {
+    yield take(authActions.SIGN_OUT);
+    yield fork(signOut);
+  }
+}
+
+
+//=====================================
+//  AUTH SAGAS
+//-------------------------------------
+
+export const authSagas = [
+  fork(watchSignIn),
+  fork(watchSignOut)
 ];
---
+++ app/firebase/config.js
@@ -0,0 +1,7 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyABOvyyjtmu4ioGemSRy4SJcjtBU5DUsqA",
+    authDomain: "rhythmroulette-78b71.firebaseapp.com",
+    databaseURL: "https://rhythmroulette-78b71.firebaseio.com",
+    storageBucket: "rhythmroulette-78b71.appspot.com"
+};
+   
