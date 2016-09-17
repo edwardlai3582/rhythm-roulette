bb8f39cfd60f1d2d3d55d0b1d24acd7e44803529
illl48 <mruncanny@gmail.com>
2016-09-16 17:57:25 -0700

    remove update

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -90,9 +90,6 @@ class App extends React.Component {
                         <Link className={styles.epLink}  to={"/ep"}>
                             EPISODES
                         </Link>
-                        <Link className={styles.epLink}  to={"/upload"}>
-                            UPLOAD
-                        </Link>
                     </nav>
                 </header>
             
